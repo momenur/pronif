e7be80c52de3491f0a885426fe7996109adf254d
momenur <momenurislam6@gmail.com>
2025-07-02 22:42:28 +0600

    sim shape card

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/home/HeroSection";
 import ServicesSection from "@/components/home/ServicesSection";
 import StepByStepSection from "@/components/home/StepByStepSection";
+import AppCard from "@/ui/AppCard";
 
 const Home = () => (
   <>
     <HeroSection />
     <ServicesSection />
     <StepByStepSection />
+    <AppCard />
   </>
 );
 
---
+++ src/ui/AppCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Heart, MessageCircle, Star } from "react-feather";
+
+const AppCard = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8 bg-gray-100">
+      <div
+        className="p-6 text-white bg-gradient-to-br from-primary to-primary-main"
+        style={{
+          clipPath: "polygon(0 0, 80% 0, 100% 20%, 100% 100%, 20% 100%, 0 80%)",
+          height: "500px",
+          width: "350px",
+        }}
+      >
+        <div className="flex flex-col justify-between h-full">
+          <h1>hello</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppCard;
