8c291bc937559a84d9e061549d8701de5482d245
momenur <momenurislam6@gmail.com>
2025-07-16 22:38:52 +0600

    theeth

--- src/components/animation/HeroSection.jsx
+++ src/components/animation/HeroSection.jsx
@@ -2,13 +2,21 @@ import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <div>
+    <div className="relative flex flex-col items-center justify-center w-full h-[60vh] md:h-[80vh] overflow-hidden bg-border-regular">
       <Image
         height={1200}
         width={700}
-        src="/images/image1.png"
+        src="/images/banner.png"
         alt="Animation Hero Background"
         className="object-cover w-full h-[60vh] md:h-[80vh]"
+      />
+
+      <Image
+        height={700}
+        width={600}
+        alt="mouth image"
+        src="/images/image6.png"
+        className="absolute bottom-0 h-auto transform -translate-x-1/2 w-[600px] left-1/2"
       />
     </div>
   );
