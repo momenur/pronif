b0c35e4a26af0be9e5262ffc1bba3b11970ea1fc
momenur <momenurislam6@gmail.com>
2025-07-16 23:22:08 +0600

    net and rotate

--- src/components/animation/HeroSection.jsx
+++ src/components/animation/HeroSection.jsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 const HeroSection = () => (
   <div className="relative overflow-hidden">
     <Image
-      height={1200}
+      height={700}
       width={700}
       src="/images/banner.png"
       alt="Animation Hero Background"
-      className="object-cover w-full h-[60vh] md:h-[80vh]"
+      className="object-cover w-full h-[700px]"
     />
 
     <Image
@@ -15,7 +15,7 @@ const HeroSection = () => (
       width={600}
       alt="mouth image"
       src="/images/mouth.png"
-      className="absolute bottom-0 h-auto transform -translate-x-1/2 w-[600px] left-1/2"
+      className="absolute bottom-10 h-auto transform -translate-x-1/2 w-[700px] left-1/2"
     />
   </div>
 );
--- src/components/animation/index.js
+++ src/components/animation/index.js
@@ -1,9 +1,26 @@
+import Image from "next/image";
 import HeroSection from "./HeroSection";
 
 const AnimationPage = () => {
   return (
     <div>
       <HeroSection />
+      <div className="relative flex items-center justify-center">
+        <Image
+          height={800}
+          width={700}
+          alt="rotate image"
+          src="/images/rotateImageBg.png"
+          className="w-1/2"
+        />
+        <Image
+          height={400}
+          width={400}
+          alt="rotate image"
+          src="/images/rotate.png"
+          className="absolute bottom-32 size-[400px] mr-10"
+        />
+      </div>
     </div>
   );
 };
