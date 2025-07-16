ee98ffc04cd52ad15128a5e53a39d0e8b9e6d417
momenur <momenurislam6@gmail.com>
2025-07-16 23:06:03 +0600

    animation heo

--- src/components/animation/HeroSection.jsx
+++ src/components/animation/HeroSection.jsx
@@ -1,25 +1,23 @@
 import Image from "next/image";
 
-const HeroSection = () => {
-  return (
-    <div className="relative flex flex-col items-center justify-center w-full h-[60vh] md:h-[80vh] overflow-hidden bg-border-regular">
-      <Image
-        height={1200}
-        width={700}
-        src="/images/banner.png"
-        alt="Animation Hero Background"
-        className="object-cover w-full h-[60vh] md:h-[80vh]"
-      />
+const HeroSection = () => (
+  <div className="relative overflow-hidden">
+    <Image
+      height={1200}
+      width={700}
+      src="/images/banner.png"
+      alt="Animation Hero Background"
+      className="object-cover w-full h-[60vh] md:h-[80vh]"
+    />
 
-      <Image
-        height={700}
-        width={600}
-        alt="mouth image"
-        src="/images/image6.png"
-        className="absolute bottom-0 h-auto transform -translate-x-1/2 w-[600px] left-1/2"
-      />
-    </div>
-  );
-};
+    <Image
+      height={700}
+      width={600}
+      alt="mouth image"
+      src="/images/mouth.png"
+      className="absolute bottom-0 h-auto transform -translate-x-1/2 w-[600px] left-1/2"
+    />
+  </div>
+);
 
 export default HeroSection;
